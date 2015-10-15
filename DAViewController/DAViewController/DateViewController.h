c3c8dca5a809f6d3a31628c6d564ffc7dda58c1e
Markus Feng <zzz1112222595@hotmail.com>
2015-10-15 11:40:58 -0400

    Added calculator references to the ViewControllers

--- DAViewController/DAViewController/DateViewController.h
+++ DAViewController/DAViewController/DateViewController.h
@@ -7,9 +7,11 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "DeathCalculator.h"
 
 @interface DateViewController : UIViewController
 
+@property DeathCalculator * calculator;
 
 
 @end
--- DAViewController/DAViewController/PickerViewController.h
+++ DAViewController/DAViewController/PickerViewController.h
@@ -7,7 +7,10 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "DeathCalculator.h"
 
 @interface PickerViewController : UIViewController
+
+@property DeathCalculator * calculator;
 
 @end
